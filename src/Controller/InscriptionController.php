177ee6f79fa5771316cd66c8e80f4a1364f7b83b
VinieAlvine <tchimoualvine1122@gmail.com>
2023-01-05 23:50:23 +0100

    page User

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -4,18 +4,46 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\InscriptionType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
 
 class InscriptionController extends AbstractController
 {
-    #[Route('/inscription', name: 'app_inscription')]
-    public function index()
+    private  $entityManager;
+   // private $passwordEncoder;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        $user= new User();
-        $form =$this->createForm(InscriptionType::class,$user);
-        $form
+        $this->entityManager = $entityManager;
+       // $this->passwordEncoder = $passwordEncoder;
+    }
+
+    #[Route('/inscription', name: 'app_inscription')]
+    public function index(Request $request,UserPasswordHasherInterface $passwordHasher)
+    {
+            $user = new User();
+            $form = $this->createForm(InscriptionType::class, $user);
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $user = $form->getData();
+                $plaintextPassword=$user->getPassword() ;
+
+               $hashedPassword = $passwordHasher->hashPassword(
+                   $user,
+                   $plaintextPassword
+               );
+        $user->setPassword($hashedPassword);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+            }
+
         return $this->render('inscription/index.html.twig',[
             'form'=>$form->createView()
         ]);
